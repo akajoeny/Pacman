563793df8f4a98052f7e641fb11af42dada012b1
Joel Nyström <e.joel.n@gmail.com>
2017-02-19 15:25:55 +0100

    Add PacmanGame.java and PacmanGameTest.java and Pom.xml

---
+++ src/main/java/se/joelnystrom/pacman/PacmanGame.java
@@ -0,0 +1,13 @@
+package se.joelnystrom.pacman;
+
+/**
+ * Hello world!
+ *
+ */
+public class PacmanGame
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
