c37d2041022949f119bad03fd219c304d2811c74
Joel Nyström <e.joel.n@gmail.com>
2017-02-25 22:25:28 +0100

    Implemented thread with run, start and stop methods

--- src/main/java/se/joelnystrom/pacman/PacmanGame.java
+++ src/main/java/se/joelnystrom/pacman/PacmanGame.java
@@ -2,7 +2,7 @@ package se.joelnystrom.pacman;
 
 import se.joelnystrom.pacman.ui.GameBoard;
 
-public class PacmanGame {
+public class PacmanGame implements Runnable {
 
     public static final int[][] maze = {
             { 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -23,22 +23,55 @@ public class PacmanGame {
             { 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
     };
 
-    public static void main( String[] args )
-    {
+    private boolean running = false;
+    private Thread thread;
+
+    private synchronized void start(){
+        if(running)
+            return;
+
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    private synchronized void stop() {
+        if(!running)
+            return;
+
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.exit(1);
+    }
+
+    public void run(){
+        //This is the game loop
+        while (running){
+
+        }
+        stop();
+    }
+
+    public static void main( String[] args ) {
+        PacmanGame pacmanGame = new PacmanGame();
+
+        GameBoard gameBoard = new GameBoard(); //Construct game board
+        gameBoard.setResizable(false);
+
+
+        pacmanGame.start();
         //Schedule a job for the event-dispatching thread:
         //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+        /*javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 pacmanGame();
             }
         });
-
-    }
-
-    private static void pacmanGame() {
-
-        GameBoard gameBoard = new GameBoard(); //Construct game board
-        gameBoard.setResizable(false);
+        */
 
     }
 }
