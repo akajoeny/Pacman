e1f0be1afac1f824b1437e3af6dc911927084676
Joel Nyström <e.joel.n@gmail.com>
2017-02-25 22:24:16 +0100

    Fixed indention in file

--- src/main/java/se/joelnystrom/pacman/ui/GameBoard.java
+++ src/main/java/se/joelnystrom/pacman/ui/GameBoard.java
@@ -101,55 +101,56 @@ public class GameBoard extends JFrame {
 
             this.add(gameLabel);
         }
-            private void movePacman(Direction d) {
-                // movePacman x, y
-                //Can only turn 90 degrees at square position
-                final int CURR_X = pacman.getX();
-                final int CURR_Y = pacman.getY();
-                int new_x = CURR_X;
-                int new_y = CURR_Y;
-                int speed = BLOCK_SIZE/4;
 
-                //System.out.print("Standing at: " + CURR_X + ", " + CURR_Y + "; ");
+        private void movePacman(Direction d) {
+            // movePacman x, y
+            //Can only turn 90 degrees at square position
+            final int CURR_X = pacman.getX();
+            final int CURR_Y = pacman.getY();
+            int new_x = CURR_X;
+            int new_y = CURR_Y;
+            int speed = BLOCK_SIZE/4;
 
-                int new_xpos = (CURR_X + d.dx*speed);
-                int new_ypos = (CURR_Y + d.dy*speed);
+            //System.out.print("Standing at: " + CURR_X + ", " + CURR_Y + "; ");
 
-                int offset_x = (new_xpos % BLOCK_SIZE)*d.dx;
-                int offset_y = (new_ypos % BLOCK_SIZE)*d.dy;
+            int new_xpos = (CURR_X + d.dx*speed);
+            int new_ypos = (CURR_Y + d.dy*speed);
 
-                if (new_xpos <= BOARD_SIZE-BLOCK_SIZE && new_ypos <= BOARD_SIZE-BLOCK_SIZE && new_xpos >= 0 && new_ypos >= 0) {
-                    //New pixel location are within the board
+            int offset_x = (new_xpos % BLOCK_SIZE)*d.dx;
+            int offset_y = (new_ypos % BLOCK_SIZE)*d.dy;
 
-                    int x_coord = new_xpos/BLOCK_SIZE; //Only right when xpos is a block corner
-                    int y_coord = new_ypos/BLOCK_SIZE; //Only right when ypos is a block corner
+            if (new_xpos <= BOARD_SIZE-BLOCK_SIZE && new_ypos <= BOARD_SIZE-BLOCK_SIZE && new_xpos >= 0 && new_ypos >= 0) {
+                //New pixel location are within the board
 
-                    if (offset_x > 0 || offset_y > 0) {
-                        //System.out.println("Offset X, Y: "+ d.dx + ", " + d.dy);
-                        //Need to check maxe at x+1 or y+1
-                        x_coord = x_coord + d.dx;
-                        y_coord = y_coord + d.dy;
-                    }
+                int x_coord = new_xpos/BLOCK_SIZE; //Only right when xpos is a block corner
+                int y_coord = new_ypos/BLOCK_SIZE; //Only right when ypos is a block corner
 
-                    if (maze[y_coord][x_coord] == 0){
-                        //System.out.println("Maze is open at: " + (x_coord+1) + ", " + (y_coord+1) + "; ");
-                        if ((CURR_Y % BLOCK_SIZE == 0) && (d.dy == 0)) {
-                            // At even pixel for Y, may turn 90, but not if wall up or down
-                            //System.out.println("right or left;");
-                            new_x = new_x + d.dx * speed;
-                        } else if ((CURR_X % BLOCK_SIZE == 0) && (d.dx == 0)) {
-                            // At even pixel for X, may turn 90
-                            //System.out.println("up or down;");
-                            new_y = new_y + d.dy * speed;
-                        }
-                    }
-
+                if (offset_x > 0 || offset_y > 0) {
+                    //System.out.println("Offset X, Y: "+ d.dx + ", " + d.dy);
+                    //Need to check maxe at x+1 or y+1
+                    x_coord = x_coord + d.dx;
+                    y_coord = y_coord + d.dy;
                 }
-                removeCharacter(pacman,getGraphics());
-                pacman.setX(new_x);
-                pacman.setY(new_y);
-                paintCharacter(pacman,getGraphics());
+
+                if (maze[y_coord][x_coord] == 0){
+                    //System.out.println("Maze is open at: " + (x_coord+1) + ", " + (y_coord+1) + "; ");
+                    if ((CURR_Y % BLOCK_SIZE == 0) && (d.dy == 0)) {
+                        // At even pixel for Y, may turn 90, but not if wall up or down
+                        //System.out.println("right or left;");
+                        new_x = new_x + d.dx * speed;
+                    } else if ((CURR_X % BLOCK_SIZE == 0) && (d.dx == 0)) {
+                        // At even pixel for X, may turn 90
+                        //System.out.println("up or down;");
+                        new_y = new_y + d.dy * speed;
+                    }
+                }
+
             }
+            removeCharacter(pacman,getGraphics());
+            pacman.setX(new_x);
+            pacman.setY(new_y);
+            paintCharacter(pacman,getGraphics());
+        }
 
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
